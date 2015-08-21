70371b7d37050fe0b78368857b291b130cc243e3
Cesar Augusto <cesaraugusto@pingobox.com.br>
2015-08-21 10:20:07 -0300

    Criação do form de contato e ajustes e melhorias

--- aplication/core/views/__init__.py
+++ aplication/core/views/__init__.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -28,7 +29,41 @@ def services(request):
 
 
 def contact(request):
+    response = {}
+
+    if request.method == "POST":
+        subject = request.POST.get('topic', '')
+        message = request.POST.get('message', '')
+        from_email = request.POST.get('email', '')
+        from_name = request.POST.get('name', '')
+
+        if subject and message and from_email and from_name:
+            text = '''
+                    Nome: %s \n
+                    Email : %s \n
+                    Mensagem: %s
+            ''' % (from_name, from_email, message)
+
+            try:
+                send_mail(subject, text, from_email, ['change@this.com'])
+                response['type'] = 'success'
+                response['title'] = 'Ok!'
+                response['message'] = 'Sua mensagem foi enviada com sucesso.'
+
+            except:
+                response['type'] = 'danger'
+                response['title'] = 'Ops!'
+                response['message'] = 'Sua mensagem não pode ser enviada,\
+                                        tente novamente mais tarde.'
+
+        else:
+            response['type'] = 'warning'
+            response['title'] = 'Atenção!'
+            response['message'] = 'Preencha o formulario,\
+                                    todos os campos são obrigatorio.'
+
     context = {
-        'title_page': 'Contate-me'
+        'title_page': 'Contate-me',
+        'response': response
     }
     return render(request, 'core/contact.html', context)
--- aplication/news_feed/views/posts_feed_blog.py
+++ aplication/news_feed/views/posts_feed_blog.py
@@ -8,7 +8,7 @@ from aplication.core.utils.url_gen import urlGen
 
 def posts_feed_blog(request, slug):
 
-    feed = get_object_or_404(FeedModels, slug=slug, status=FeedModels.ACTIVE)
+    feed = get_object_or_404(FeedModels, slug=slug)
     news_list = NewsFeedModels.objects.filter(feed=feed)
 
     news_paginator = Paginator(news_list, 10)

30eec5ee386b0fefae7fc1b4a18cba6b8e984bb1
Cesar Bruschetta <cesar.bruschetta@lexisnexis.com>
2018-03-12 13:29:57 -0300

    Ajusts in portfolio

--- aplication/core/views/portfolio.py
+++ aplication/core/views/portfolio.py
@@ -11,148 +11,148 @@ def portfolio(request):
 
     context['portfolio_items'] = [
         {
-            'url': '/Sites/TI_Comunicacoes',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site1.png',
+            'url': 'http://sites.cesarbruschetta.com.brTI_Comunicacoes',
+            'image': '/static/core/portfolio/site1.png',
             'title': 'TI Comunicações'
         },
         {
-            'url': '/Sites/Golden_Gates',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site12.png',
+            'url': 'http://sites.cesarbruschetta.com.brGolden_Gates',
+            'image': '/static/core/portfolio/site12.png',
             'title': 'Golden Gate'
         },
         {
-            'url': '/Sites/Music_in_Hand/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site2.png',
+            'url': 'http://sites.cesarbruschetta.com.brMusic_in_Hand/',
+            'image': '/static/core/portfolio/site2.png',
             'title': 'Music In Hand'
         },
         {
-            'url': '/Sites/U2',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site3.png',
+            'url': 'http://sites.cesarbruschetta.com.brU2',
+            'image': '/static/core/portfolio/site3.png',
             'title': 'U2'
         },
         {
-            'url': '/Sites/Cab_Inf_Old',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site14.png',
+            'url': 'http://sites.cesarbruschetta.com.brCab_Inf_Old',
+            'image': '/static/core/portfolio/site14.png',
             'title': 'Cab Informatica - Old'
         },
         {
-            'url': '/Sites/Physis',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site4.png',
+            'url': 'http://sites.cesarbruschetta.com.brPhysis',
+            'image': '/static/core/portfolio/site4.png',
             'title': 'Physis Jardinagem - Old'
         },
         {
-            'url': '/Sites/Servicosrn_old',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site5.png',
-            'title': 'Serviços RN - Old'
+            'url': 'http://sites.cesarbruschetta.com.brServicosrn_old',
+            'image': '/static/core/portfolio/site5.png',
+            'title': 'ServiÃ§os RN - Old'
         },
         {
-            'url': '/Sites/Viage_PaulaTur/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site6.png',
+            'url': 'http://sites.cesarbruschetta.com.brViage_PaulaTur/',
+            'image': '/static/core/portfolio/site6.png',
             'title': 'Paula Tur'
         },
         {
-            'url': '/Sites/Lords_of_Games/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site8.png',
+            'url': 'http://sites.cesarbruschetta.com.brLords_of_Games/',
+            'image': '/static/core/portfolio/site8.png',
             'title': 'Lords Of Games'
         },
         {
-            'url': '/Sites/PRB_Virtual/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site7.png',
+            'url': 'http://sites.cesarbruschetta.com.brPRB_Virtual/',
+            'image': '/static/core/portfolio/site7.png',
             'title': 'PRB Virtual'
         },
         {
-            'url': '/Sites/Wood_Center/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site9.png',
+            'url': 'http://sites.cesarbruschetta.com.brWood_Center/',
+            'image': '/static/core/portfolio/site9.png',
             'title': 'Wood Center'
         },
         {
-            'url': '/Sites/JLS_Divisorias/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site10.png',
+            'url': 'http://sites.cesarbruschetta.com.brJLS_Divisorias/',
+            'image': '/static/core/portfolio/site10.png',
             'title': 'JLS Divisorias - Old'
         },
         {
-            'url': '/Sites/Speedy_AM',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site11.png',
+            'url': 'http://sites.cesarbruschetta.com.brSpeedy_AM',
+            'image': '/static/core/portfolio/site11.png',
             'title': 'Speedy AutoMotors'
         },
         {
-            'url': '/Sites/Vidal_Associados',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site13.png',
+            'url': 'http://sites.cesarbruschetta.com.brVidal_Associados',
+            'image': '/static/core/portfolio/site13.png',
             'title': 'Vidal Associados'
         },
         {
-            'url': '/Sites/Jogos_Flash',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site15.png',
+            'url': 'http://sites.cesarbruschetta.com.brJogos_Flash',
+            'image': '/static/core/portfolio/site15.png',
             'title': 'Jogos Flash'
         },
         {
             'url': 'http://juquinhacontabel.herokuapp.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site16.png',
+            'image': '/static/core/portfolio/site16.png',
             'title': 'Juquinha Contabil'
         },
         {
             'url': 'http://www.servicosrn.com.br/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site5.png',
+            'image': '/static/core/portfolio/site5.png',
             'title': 'Serviços RN'
         },
         {
-            'url': '/Sites/Xoops_Site/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site18.png',
+            'url': 'http://sites.cesarbruschetta.com.brXoops_Site/',
+            'image': '/static/core/portfolio/site18.png',
             'title': 'Xoop Media'
         },
         {
-            'url': '/Sites/Elegant_Design/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site19.png',
+            'url': 'http://sites.cesarbruschetta.com.brElegant_Design/',
+            'image': '/static/core/portfolio/site19.png',
             'title': 'Elegant Design'
         },
         {
-            'url': '/Sites/Cab_Inf_New',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site20.png',
+            'url': 'http://sites.cesarbruschetta.com.brCab_Inf_New',
+            'image': '/static/core/portfolio/site20.png',
             'title': 'Cab Informatica'
         },
         {
             'url': 'http://djangoportal.herokuapp.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site21.png',
+            'image': '/static/core/portfolio/site21.png',
             'title': 'Django Portal'
         },
         {
             'url': 'http://portal-in-gae.appspot.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site23.png',
+            'image': '/static/core/portfolio/site23.png',
             'title': 'Captive Green - GAE'
         },
         {
             'url': 'http://feeds-in-gae.appspot.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site22.png',
+            'image': '/static/core/portfolio/site22.png',
             'title': 'Global Feeds - GAE'
         },
         {
-            'url': '/Sites/King_of_Games',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site24.png',
+            'url': 'http://sites.cesarbruschetta.com.brKing_of_Games',
+            'image': '/static/core/portfolio/site24.png',
             'title': 'King of Games'
         },
         {
             'url': 'http://portaldjangocms.herokuapp.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site25.png',
+            'image': '/static/core/portfolio/site25.png',
             'title': 'Django CMS'
         },
         {
             'url': 'http://django-in-gae.appspot.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site26.png',
+            'image': '/static/core/portfolio/site26.png',
             'title': 'Django in GAE'
         },
         {
             'url': 'http://portal-nodejs.herokuapp.com/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site27.png',
+            'image': '/static/core/portfolio/site27.png',
             'title': 'Portal NodeJS'
         },
         {
             'url': 'http://cab.cesarbruschetta.com.br/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site28.png',
+            'image': '/static/core/portfolio/site28.png',
             'title': u'Cab Informática'
         },
         {
             'url': 'http://plone.cesarbruschetta.com.br/',
-            'image': 'http://www.cesarbruschetta.com.br/media/portfolio/site29.png',
+            'image': '/static/core/portfolio/site29.png',
             'title': 'Cesar Augusto - Old'
         },
     ]
